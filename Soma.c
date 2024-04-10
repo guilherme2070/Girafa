9a474712ad67aa6ad56046de74ce3b63423ecedb
guilherme2070 <160781764+guilherme2070@users.noreply.github.com>
2024-04-10 16:01:56 -0300

    Add files via upload

---
+++ Soma.c
@@ -0,0 +1,19 @@
+void main() {
+    float n1,n2,n3,n4,soma;
+
+    printf("digite o valor de 1");
+    scanf("%f",&n1);
+    printf("digite o valor de 2");
+    scanf("%f",&n2);
+    printf("digite o valor de 3");
+    scanf("%f",&n3);
+    printf("digite o valor de 4");
+    scanf("%f",&n4);
+    
+    soma = n1+n2+n3+n4;
+
+    printf("o resultado sera; %f");
+
+
+
+}
